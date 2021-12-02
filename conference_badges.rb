daf41968f1a2cc66f0cc9c25d09962b200639de6
Goldie <goldiejackson25@yahoo.com>
2021-12-01 20:43:34 -0600

    commit tree f9399396c5c00d0ab0b2039b30c44bb763040468

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
 # Write your code here.
+require 'pry'
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    array.map {|x| badge_maker(x)}
+end
+
+def assign_rooms(speakers_array)
+    speakers_array.map.with_index(1) {|x, index| "Hello, #{x}! You'll be assigned to room #{index}!"}
+end
+
+def printer(array_of_names)
+    array_of_names.each {|x| puts badge_maker(x)}
+    #binding.pry
+    assign_rooms(array_of_names).each {|x| puts x}
+end
